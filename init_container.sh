8a7ad99bc3b66ac1db3f869d1e62552bbcaaf2cd
Srikanth Sureddy <srsuredd@microsoft.com>
2017-04-27 10:51:46 -0500

    Changes for tomcat conf directory

--- init_container.sh
+++ init_container.sh
@@ -4,12 +4,15 @@ service ssh start
 if [ ! -d "/home/site/conf" ]; then
 	echo "Info: Tomcat configuration directory is not available. Creating one"
 	mkdir -p /home/site/conf
-	cp -R /usr/local/tomcat/conf/. /home/site/conf/
-	rm -fr /usr/local/tomcat/conf 
-	ln -s /home/site/conf /usr/local/tomcat/conf
+	cp -R /usr/local/tomcat/conf/. /home/site/conf/	
 else
 	echo "Info: Tomcat configuration directory already exists"
 fi
+
+rm -rf /usr/local/tomcat/conf 
+ln -s /home/site/conf /usr/local/tomcat/conf
+
+chown -R root:root /home/site/conf
 
 cd /home/site/wwwroot
 
