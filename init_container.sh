a13e856cf3286b1f10f16192eb048515417c853f
Srikanth Sureddy <srsuredd@microsoft.com>
2017-04-27 09:21:41 -0500

    Modified init_container.sh for configuration

--- init_container.sh
+++ init_container.sh
@@ -5,7 +5,7 @@ if [ ! -d "/home/site/conf" ]; then
 	echo "Info: Tomcat configuration directory is not available. Creating one"
 	mkdir -p /home/site/conf
 	cp -R /usr/local/tomcat/conf/. /home/site/conf/
-	rm -f /usr/local/tomcat/conf 
+	rm -fr /usr/local/tomcat/conf 
 	ln -s /home/site/conf /usr/local/tomcat/conf
 else
 	echo "Info: Tomcat configuration directory already exists"
